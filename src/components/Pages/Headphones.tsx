98d32a6fc1421cc42d04f20055c48b6996f1d1fd
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-07-25 20:11:27 +0400

    add headphones page

--- src/components/Pages/Headphones.tsx
+++ src/components/Pages/Headphones.tsx
@@ -1,12 +1,58 @@
 import React from "react";
 import ProductsHero from "../sub-components/productsPage/ProductsHero";
+import ProductSection from "../sub-components/productsPage/ProductSection";
+import styled from "styled-components";
+import data from "../../data.json";
+import Navigation from "../sub-components/home/Navigation";
+import MainBlog from "../sub-components/home/MainBlog";
 
 function Headphones() {
+  // Filter the data to get all products with category "headphones"
+  const headphoneProducts = data.filter(
+    (item) => item.category === "headphones"
+  );
+  console.log(headphoneProducts, "diidbwidvwbo");
+  const productIndex = headphoneProducts[0];
+  console.log(productIndex);
+
   return (
     <div>
       <ProductsHero text="HEADPHONES" />
+      <Content>
+        <Wrapper>
+          {headphoneProducts.length > 0 ? (
+            headphoneProducts.map((item, index) => (
+              <ProductSection
+                key={item.id}
+                img={item.image.desktop} // Assuming you want to use the desktop image
+                name={item.name}
+                description={item.description}
+                index={index}
+                id={item.id}
+                category={item.category}
+              />
+            ))
+          ) : (
+            <div>No products available</div>
+          )}
+          <Navigation />
+          <MainBlog />
+        </Wrapper>
+      </Content>
     </div>
   );
 }
 
 export default Headphones;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  // Add your desired styles here
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/sub-components/productsPage/ProductSection.tsx
+++ src/components/sub-components/productsPage/ProductSection.tsx
@@ -1,7 +1,130 @@
 import React from "react";
+import styled from "styled-components";
+import { defaultTheme } from "../../utils/defaultTheme";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function ProductSection() {
-  return <div>ProductSection</div>;
+interface ProductSectionProps {
+  img: string;
+  name: string;
+  description: string;
+  index: number;
+  id: number;
+  category: string;
+}
+
+function ProductSection({
+  img,
+  name,
+  description,
+  index,
+  id,
+  category,
+}: ProductSectionProps) {
+  // Determine if index is odd or even
+  const isOdd = index % 2 !== 0;
+  const { phathName } = useParams();
+  console.log(phathName);
+
+  return (
+    <InformationContainer isOdd={isOdd}>
+      <MainImage>
+        <img src={img} alt="" />
+      </MainImage>
+      <Info>
+        {index === 0 && <Title>NEW PRODUCT</Title>}
+        <ProductName>{name}</ProductName>
+        <Description>{description}</Description>
+        <Box>
+          <AddCart to={`/${category}/${id}`}>ADD TO CART</AddCart>
+        </Box>
+      </Info>
+    </InformationContainer>
+  );
 }
 
 export default ProductSection;
+
+const InformationContainer = styled.div<{ isOdd: boolean }>`
+  width: 1440px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 56px;
+  margin-bottom: 160px;
+  padding: 0 20px;
+  flex-direction: ${(props) => (props.isOdd ? "row-reverse" : "row")};
+`;
+
+const MainImage = styled.div`
+  padding: 65px 95px 108px 95px;
+  background-color: ${defaultTheme.colors.flashwite};
+  img {
+    width: 349.238px;
+    height: 386px;
+    flex-shrink: 0;
+  }
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  color: ${defaultTheme.colors.peru};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 10px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+`;
+
+const ProductName = styled.h1`
+  color: ${defaultTheme.colors.black};
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 44px; /* 110% */
+  letter-spacing: 1.429px;
+  text-transform: uppercase;
+  margin-bottom: 32px;
+  width: 445px;
+`;
+
+const Description = styled.p`
+  width: 445px;
+  color: ${defaultTheme.colors.black};
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 166.667% */
+  opacity: 0.5;
+  margin-bottom: 32px;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  margin-top: 50px;
+`;
+
+const AddCart = styled(Link)`
+  color: ${defaultTheme.colors.white};
+  background-color: ${defaultTheme.colors.peru};
+  text-align: center;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  border: 0;
+  padding: 15px 30.5px 15px 34.5px;
+  cursor: pointer;
+  text-decoration: none;
+`;
