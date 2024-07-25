b5966723db236757fda49b9d54244541beef2f5e
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-07-25 20:20:51 +0400

    add earphones page for desktop

--- src/components/Pages/Earphones.tsx
+++ src/components/Pages/Earphones.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import ProductsHero from "../sub-components/productsPage/ProductsHero";
+import ProductSection from "../sub-components/productsPage/ProductSection";
+import styled from "styled-components";
+import data from "../../data.json";
+import Navigation from "../sub-components/home/Navigation";
+import MainBlog from "../sub-components/home/MainBlog";
 
 function Earphones() {
-  return <div>Earphones</div>;
+  // Filter the data to get all products with category "headphones"
+  const headphoneProducts = data.filter(
+    (item) => item.category === "earphones"
+  );
+  console.log(headphoneProducts, "earphones");
+  const productIndex = headphoneProducts[0];
+  console.log(productIndex);
+  return (
+    <div>
+      <ProductsHero text="EARPHONES" />
+      <Content>
+        <Wrapper>
+          {headphoneProducts.length > 0 ? (
+            headphoneProducts.map((item, index) => (
+              <ProductSection
+                key={item.id}
+                img={item.image.desktop} // Assuming you want to use the desktop image
+                name={item.name}
+                description={item.description}
+                index={index}
+                id={item.id}
+                category={item.category}
+              />
+            ))
+          ) : (
+            <div>No products available</div>
+          )}
+          <Navigation />
+          <MainBlog />
+        </Wrapper>
+      </Content>
+    </div>
+  );
 }
 
 export default Earphones;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  // Add your desired styles here
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
